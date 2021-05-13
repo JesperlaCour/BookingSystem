3f52ba8c09d6244903966cc2d90b3d2a708d549d
Marc3411 <64631831+Marc3411@users.noreply.github.com>
2021-05-13 12:09:38 +0200

    APIFirst

---
+++ ResourceAPI/ResourceAPI/Data/ResourceContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ResourceAPI.Models;
+
+namespace ResourceAPI.Data
+{
+    public class ResourceContext : DbContext
+    {
+        
+            public DbSet<Resource> Resources { get; set; }
+            public DbSet<Category> Categories { get; set; }
+            public DbSet<SubCategory> SubCategories { get; set; }
+
+    }
+}
