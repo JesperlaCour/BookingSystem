b86e66c18ea221ea82c1f26b123eee74403eb207
Marc3411 <64631831+Marc3411@users.noreply.github.com>
2021-05-13 15:04:12 +0200

    APIv2

--- ResourceAPI/ResourceAPI/Data/ResourceContext.cs
+++ ResourceAPI/ResourceAPI/Data/ResourceContext.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ResourceAPI.Models;
 
 namespace ResourceAPI.Data
 {
     public class ResourceContext : DbContext
     {
-        
-            public DbSet<Resource> Resources { get; set; }
+
+
+        public ResourceContext(DbContextOptions<ResourceContext> options)
+            : base(options)
+        {
+
+        }
+
+    public DbSet<Resource> Resources { get; set; }
             public DbSet<Category> Categories { get; set; }
             public DbSet<SubCategory> SubCategories { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=tcp:lacour.database.windows.net,1433;Initial Catalog=DelPinResource;Persist Security Info=False;User ID=Jesper_laCour;Password=Azure1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
 
     }
 }
--- ResourceAPI/ResourceAPI/Startup.cs
+++ ResourceAPI/ResourceAPI/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ResourceAPI.Data;
 
 namespace ResourceAPI
 {
@@ -32,6 +34,9 @@ namespace ResourceAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ResourceAPI", Version = "v1" });
             });
+
+            services.AddDbContext<ResourceContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Db")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
