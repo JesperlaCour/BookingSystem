caf56557be2f6590c0dc8075ad598d02c4ecb8fd
RagnarSR8210 <ragnarsvenson@hotmail.com>
2021-05-18 11:49:36 +0200

    New Project With Authentication
    Ny Mvc mew auth.

---
+++ DelPinBookingV2/DelPinBookingV2/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DelPinBookingV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    }
+}
