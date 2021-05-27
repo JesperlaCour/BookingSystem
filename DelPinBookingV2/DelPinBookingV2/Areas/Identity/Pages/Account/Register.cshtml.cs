53c13eff687413cdf0002864d44ddd2b0fcb9122
Marc3411 <64631831+Marc3411@users.noreply.github.com>
2021-05-27 10:02:35 +0200

    CustomerRoleAssignmentOnRegister

--- DelPinBookingV2/DelPinBookingV2/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DelPinBookingV2/DelPinBookingV2/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -89,6 +89,7 @@ namespace DelPinBookingV2.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    _userManager.AddToRoleAsync(user, "Customer").Wait();
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
