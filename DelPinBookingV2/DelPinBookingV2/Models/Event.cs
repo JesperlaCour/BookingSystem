1dde857654b68712501e69306ec04e30e31ea6ee
Marc3411 <64631831+Marc3411@users.noreply.github.com>
2021-05-28 11:10:23 +0200

    DataNotationEmailAdded

--- DelPinBookingV2/DelPinBookingV2/Models/Event.cs
+++ DelPinBookingV2/DelPinBookingV2/Models/Event.cs
@@ -8,6 +8,7 @@ namespace DelPinBookingV2.Models
         public int Id { get; set; }
         public int ResourceId { get; set; }
         [DataType(DataType.Text)]
+        [EmailAddress]
         public string UserName { get; set; }
         public bool AllDay { get; set; } = false;
         [DataType(DataType.Text)]
