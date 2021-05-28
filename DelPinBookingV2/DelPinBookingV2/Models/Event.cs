6a4a3788b9016485e0db6b36a390dc5a5ee214b8
Marc3411 <64631831+Marc3411@users.noreply.github.com>
2021-05-28 10:26:00 +0200

    DataAnnotationAddedToModels

--- DelPinBookingV2/DelPinBookingV2/Models/Event.cs
+++ DelPinBookingV2/DelPinBookingV2/Models/Event.cs
@@ -5,17 +5,18 @@ namespace DelPinBookingV2.Models
 {
     public class Event
     {
+        [Key]
         public int Id { get; set; }
         public int ResourceId { get; set; }
+        [DataType(DataType.Text)]
         public string UserName { get; set; }
         public bool AllDay { get; set; } = false;
+        [DataType(DataType.Text)]
         public string Title { get; set; }
         public string Start { get; set; }
         public string End { get; set; }
+        [DataType(DataType.Text)]
         public string AddressStr { get; set; }
-
-        public bool delivery { get; set; }
-        public string deliveryComments { get; set; }
 
         [Timestamp]
         public byte[] RowVersion { get; set; }
--- DelPinBookingV2/DelPinBookingV2/Models/Resource.cs
+++ DelPinBookingV2/DelPinBookingV2/Models/Resource.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,9 @@ namespace DelPinBookingV2.Models
     public class Resource
     {
        
-
+        [Key]
         public int Id { get; set; }
+        [DataType(DataType.Text)]
         public string Title { get; set; }
         public string groupId { get; set; }
 
